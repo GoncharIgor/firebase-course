edd7218ade0f39c17f13b63d346817facf530d08
igorgo <igorgo@backbase.com>
2020-12-23 14:11:56 +0100

    login was commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AngularFireAuth} from '@angular/fire/auth';
+import {Observable, of} from 'rxjs';
+import {map} from 'rxjs/operators';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+    isLoggedIn$: Observable<boolean>;
+    pictureUrl$: Observable<string>;
+
+    constructor(private angularFireAuth: AngularFireAuth) {
+    }
+
+    ngOnInit(): void {
+        this.angularFireAuth.authState.subscribe(user => console.log(user));
+        this.isLoggedIn$ = this.angularFireAuth.authState.pipe(map(user => !!user));
+        this.pictureUrl$ = this.angularFireAuth.authState.pipe(map(user => user?.photoURL));
+    }
+
     logout() {
+        this.angularFireAuth.auth.signOut();
+        this.isLoggedIn$ = of(false);
     }
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,36 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, NgZone, OnDestroy, OnInit} from '@angular/core';
 import * as firebaseui from 'firebaseui';
 import * as firebase from 'firebase/app';
 import {AngularFireAuth} from '@angular/fire/auth';
+import {Router} from '@angular/router';
 
 @Component({
-  selector: 'login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+    selector: 'login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
-  ui: firebaseui.auth.AuthUI;
+export class LoginComponent implements OnInit, OnDestroy {
+    ui: firebaseui.auth.AuthUI;
 
-  constructor(private angularFireAuth: AngularFireAuth) { }
+    constructor(private angularFireAuth: AngularFireAuth,
+                private router: Router,
+                private ngZone: NgZone) {
+    }
 
-  ngOnInit() {
-    const uiConfig = {
-      signInOptions: [
-          firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-          firebase.auth.EmailAuthProvider.PROVIDER_ID
-      ],
-      callbacks: {
-        // good practice to bid callback f()s
-        signInSuccessWithAuthResult: this.onLoginSuccessful.bind(this)
-      }
-    };
+    ngOnInit() {
+        const uiConfig = {
+            signInOptions: [
+                firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+                firebase.auth.EmailAuthProvider.PROVIDER_ID
+            ],
+            callbacks: {
+                // good practice to bid callback f()s
+                signInSuccessWithAuthResult: this.onLoginSuccessful.bind(this)
+            }
+        };
 
-    this.ui = new firebaseui.auth.AuthUI(this.angularFireAuth.auth);
-    this.ui.start('#firebase-auth-container', uiConfig); // css selector
-  }
+        this.ui = new firebaseui.auth.AuthUI(this.angularFireAuth.auth);
+        this.ui.start('#firebase-auth-container', uiConfig); // css selector
+    }
 
-  onLoginSuccessful() {
+    ngOnDestroy() {
+      this.ui.delete();
+    }
 
-  }
-
+  onLoginSuccessful(result) {
+        this.ngZone.run(() => this.router.navigateByUrl('/courses'));
+    }
 }
